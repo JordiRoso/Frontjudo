91f7993d9228ae702ba5ef20bd169d27d5a1f220
Jordi <jordirosado@jordirosado.com>
2023-02-10 09:10:22 +0100

    Feat: creado el Register

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,3 +1,122 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useInsertionEffect} from "react";
 import AuthService from "../../_services/AuthService";
+import TokenStorageService from "../../_services/TokenStorageService";
+import { validateLoginFormValues } from "../../_helpers/form-utilities";
+
+export default function Register() {
+    const initialValues = {
+        email: "",
+        password: "",
+        name: "",
+    };
+    const navigate = useNavigate();
+
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    useInsertionEffect(() => {
+        const user = {
+            email: formValues.email,
+            password: formValues.password,
+            name: formValues.name,
+        };
+        if (Object.keys(formErrors).length == 0 && isSubmit) {
+            console.log("Register...");
+            Register(user);
+        }
+        console.log("useEffect", formErrors);
+    }, [formErrors]);
+
+    const register = async (user) => {
+        try{
+            const res = await AuthService.register(user);
+            console.log(res.data);
+            TokenStorageService.saveToken(res.data.token);
+            navigate("/login");
+        } catch (error){
+            console.log(error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const {name, value } = e.target;
+        setFormValues({
+            ...formValues,
+
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit");
+        setFormErrors (validateLoginFormValues(formValues));
+        console.log("hanfle", formErrors);
+        setIsSubmit(true);
+    };
+
+    return (
+        <div>
+          <div className="container pt-5 col-lg-3">
+            <h2>Register</h2>
+    
+            {/* <pre className="text-start">
+              {JSON.stringify(formValues, undefined, 2)}
+            </pre> */}
+    
+            <form className="text-start" noValidate onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  value={formValues.email}
+                  onChange={handleChange}
+                />
+                <div className="form-text form-text-error">{formErrors.email}</div>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  value={formValues.password}
+                  onChange={handleChange}
+                />
+                <div className="form-text form-text-error">
+                  {formErrors.password}
+                </div>
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                  type="name"
+                  name="name"
+                  className="form-control"
+                  value={formValues.name}
+                  onChange={handleChange}
+                />
+                {/* <div className="form-text form-text-error">
+                  {formErrors.password}
+                </div> */}
+              </div>
+    
+              <div className="d-grid gap-2">
+                <button
+                  type="submit"
+                  className="btn btn-primary text-white fw-bold"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+    
+          {/* <button onClick={() => login(credentials)}>Enviar login</button> */}
+        </div>
+      );
+}
 
