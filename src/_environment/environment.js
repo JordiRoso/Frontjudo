b11c6c4c1a4523ec58628411c2054f92fe7e7af8
Jordi <jordirosado@jordirosado.com>
2023-03-02 12:06:17 +0100

    fix: actualizado environment

--- src/_environment/environment.js
+++ src/_environment/environment.js
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
 
 //API
-BASE_API_URL: `http://localhost:3000`,
+// BASE_API_URL: `http://localhost:3000`,
+BASE_API_URL: `https://frontjudo-production.up.railway.app`,
 };
